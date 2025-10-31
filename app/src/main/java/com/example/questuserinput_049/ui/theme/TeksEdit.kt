167fdf23803457830c7685a6549d3767c6b06c17
vararais <varadigmaisnann@gmail.com>
2025-10-31 14:53:28 +0700

    Membuat variabel nama, alamat, dan text Jk serta val untuk gender

--- app/src/main/java/com/example/questuserinput_049/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_049/ui/theme/TeksEdit.kt
@@ -1,2 +1,30 @@
 package com.example.questuserinput_049.ui.theme
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    var textNama by remember {mutableStateOf(value = "")}
+    var textAlamat by remember {mutableStateOf(value = "")}
+    var textJK by remember {mutableStateOf(value = "")}
+
+    var nama by remember {mutableStateOf(value = "")}
+    var alamat by remember {mutableStateOf(value = "")}
+    var jenis by remember {mutableStateOf(value = "")}
+
+    val gender: List<String> = listOf("Laki Laki","Perempuan")
+
+
+}
