65abbdf14abfa3f6064d63379cdf262421d79012
vararais <varadigmaisnann@gmail.com>
2025-10-31 15:52:55 +0700

    Memanggil fungsi ke main

--- app/src/main/java/com/example/questuserinput_049/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput_049/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.questuserinput_049.ui.theme.FormDataDiri
 import com.example.questuserinput_049.ui.theme.QuestUserInput_049Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput_049Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
