93ebcf1860a344d2381c96dd96d66728648b2167
vararais <varadigmaisnann@gmail.com>
2025-10-31 15:04:42 +0700

    Membuat isi OutlinedTextField , membuat row dan RadioButton

--- app/src/main/java/com/example/questuserinput_049/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_049/ui/theme/TeksEdit.kt
@@ -3,9 +3,16 @@ package com.example.questuserinput_049.ui.theme
 import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -30,8 +37,30 @@ fun FormDataDiri(modifier: Modifier
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally) {
         OutlinedTextField(
-
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkao") },
+            onValueChange = {
+                textNama = it
+            }
         )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
+                }
+            }
+        }
     }
 
 
