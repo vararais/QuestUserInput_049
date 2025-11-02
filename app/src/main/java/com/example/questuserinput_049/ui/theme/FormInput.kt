3b519893238a87f5dcde17360648e910185c7e3f
vararais <varadigmaisnann@gmail.com>
2025-11-02 13:33:20 +0700

    Membuat fungsi FormRegistrasi

--- app/src/main/java/com/example/questuserinput_049/ui/theme/FormInput.kt
+++ app/src/main/java/com/example/questuserinput_049/ui/theme/FormInput.kt
@@ -1,2 +1,22 @@
 package com.example.questuserinput_049.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.questuserinput_049.R
+
+@Composable
+fun FormRegistrasi(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(dimensionResource(R.dimen.padding_default)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
