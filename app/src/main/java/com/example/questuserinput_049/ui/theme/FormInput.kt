ac33a35632f13a783c32c6d74e58625441f5aa93
vararais <varadigmaisnann@gmail.com>
2025-11-02 13:43:01 +0700

    Membuat state management untuk form dan menyesuaikan strings

--- app/src/main/java/com/example/questuserinput_049/ui/theme/FormInput.kt
+++ app/src/main/java/com/example/questuserinput_049/ui/theme/FormInput.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -11,12 +15,27 @@ import com.example.questuserinput_049.R
 
 @Composable
 fun FormRegistrasi(modifier: Modifier = Modifier) {
+
+    var nama by remember { mutableStateOf("") }
+    var kotaAsal by remember { mutableStateOf("") }
+    var TTL by remember { mutableStateOf("") }
+    var RT by remember { mutableStateOf("") }
+    var RW by remember { mutableStateOf("") }
+    var umur by remember { mutableStateOf("") }
+
+    var jenisKelamin: List<String> = listOf("Laki-laki","Perempuan")
+
+    var isSetuju by remember { mutableStateOf(false) }
+
+    var showDialog by remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier
             .padding(dimensionResource(R.dimen.padding_default)),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
 
     }
 }
